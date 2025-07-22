be7f22a0beec4ee6cbfcb1943d5f5ea6275f021b
Rasibul <rasibul179@gmail.com>
2025-07-22 23:44:09 +0600

    solve create booking error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`✅ Server listening on http://localhost:${PORT}`);
