4e9d485bf0c4d11b414f3c82891e14d546aa3afa
Rasibul <rasibul179@gmail.com>
2025-07-22 22:51:04 +0600

    setup backend for bokking-backend-system

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+// import router from "./routes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// API entry point
+// app.use("/api", router);
+
+app.get("/", (_, res) => {
+    res.send("🚀 Server is running!");
+});
+
+export default app;
